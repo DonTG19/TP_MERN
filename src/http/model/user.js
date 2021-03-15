bc4fec7133db9669f0156cad0a897d6214e0ea6c
Don TG <govanetsianga79@gmail.com>
2021-03-15 23:43:33 +0100

    mise en place du backend

---
+++ src/http/model/user.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 3
+  },
+  gender: {
+      type: String,
+      required: true,
+      enum : ['male','female']
+  },
+  dob: {
+      type: Date,
+      required: true
+  },
+  news: {
+      type: Boolean,
+      required: true
+  },
+  email: {
+      type: String,
+      required: true
+  },
+  photo: {
+    type: String,
+    required: false
+  }
+}, {
+  timestamps: true,
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
---
+++ src/http/routes/users.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+let User = require('../model/user');
+
+router.route('/users').get((req, res) => {
+  User.find()
+    .then(users => res.json(users))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/users').post((req, res) => {
+  const username = req.body.username;
+  const gender = req.body.gender;
+  const dob = req.body.dob;
+  const news = req.body.news;
+  const email = req.body.email;
+  const photo = req.body.photo;
+
+  const newUser = new User({
+      username,
+      gender,
+      dob,
+      news,
+      email,
+      photo
+  });
+
+  newUser.save()
+    .then(() => res.json('User added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
