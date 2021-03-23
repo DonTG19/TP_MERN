545b81d3a07fc457af2b5b1d9705d04e824ce7ae
Don TG <govanetsianga79@gmail.com>
2021-03-23 18:18:04 +0100

    Update CheckBox.js

--- src/js/components/CheckBox.js
+++ src/js/components/CheckBox.js
@@ -1,8 +1,30 @@
-function CheckBox(props){
-    return (
-        <div className="form-check">
-            <input className="form-check-input" type="checkbox" value="" id={props.id} />
-            <label className="form-check-label" htmlFor={props.id}>{props.label}</label>
-        </div>
-    );
+class CheckBox extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e){
+        if(e.target.checked)
+            this.props.onValueChange(e.target.value);
+        else
+            this.props.onValueChange(0);
+    }
+
+    render(){
+        return (
+            <div className="form-check">
+                <input 
+                    onChange={this.handleChange}
+                    className="form-check-input" 
+                    value={this.props.value} 
+                    type="checkbox" 
+                    checked={this.props.checked == this.props.value}
+                    id={this.props.id}
+                />
+                <label className="form-check-label" htmlFor={this.props.id}>{this.props.label}</label>
+            </div>
+        );
+    }
 }
