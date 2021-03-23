1bbfcb3a63b3eef2bbee81f527be89767f28c531
Don TG <govanetsianga79@gmail.com>
2021-03-23 18:18:28 +0100

    Update Button.js

--- src/js/components/Button.js
+++ src/js/components/Button.js
@@ -1,7 +1,19 @@
-function Button(props){
-    return (
-        <div className="button">
-            <button data-bs-toggle="modal" data-bs-target="#staticBackdrop">{props.text}</button>
-        </div>
-    );
+class Button extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(){
+        this.props.click();
+    }
+
+    render(){
+        return (
+            <div className="button">
+                <button disabled={this.props.disabled} onClick={this.handleClick}>{this.props.text}</button>
+            </div>
+        );
+    }
 }
