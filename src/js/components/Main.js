1ec0f68a3fc5edf257b78ea1e9dba72c1c5c2242
Don TG <govanetsianga79@gmail.com>
2021-03-23 18:17:15 +0100

    Update Main.js

--- src/js/components/Main.js
+++ src/js/components/Main.js
@@ -3,25 +3,49 @@ class Main extends React.Component{
         super(props);
         this.state = {users: [], userNumber: 0};
         this.modal = React.createRef();
+        this.modalInfosUser = React.createRef();
 
 
         this.getUserListe = this.getUserListe.bind(this);
         this.displayNewUser = this.displayNewUser.bind(this);
         this.removeUser = this.removeUser.bind(this);
         this.showUpdateUserUI = this.showUpdateUserUI.bind(this);
-        this.getUser = this.getUser.bind(this);
+        this.getUserForUpdate = this.getUserForUpdate.bind(this);
+        this.getUserForInfos = this.getUserForInfos.bind(this);
         this.updateUser = this.updateUser.bind(this);
         this.getNumberOfUser = this.getNumberOfUser.bind(this);
         this.requestForUsers = this.requestForUsers.bind(this);
+        this.requestForCountUsers = this.requestForCountUsers.bind(this);
+        this.onFiltered = this.onFiltered.bind(this);
+        this.showInfosUserUI = this.showInfosUserUI.bind(this);
+        this.onFetched = this.onFetched.bind(this);
     }
 
     componentDidMount(){
         this.requestForUsers();
-        makeRequest('/countusers', 'GET', this.getNumberOfUser);
+        this.requestForCountUsers();
     }
 
-    requestForUsers(page = 1){
-        makeRequest('/users/' + page +'/10', 'GET', this.getUserListe);
+    requestForUsers(page = 1, search = '', gender = 0, dob = 0){
+        makeRequest('/users/' + page +'/10?search=' + search + '&gender=' + gender + "&dob=" + dob, 'GET', this.getUserListe);
+    }
+
+    requestForCountUsers(search = '', gender = 0, dob = 0){
+        makeRequest('/countusers?search=' + search + '&gender=' + gender + "&dob=" + dob, 'GET', this.getNumberOfUser);
+    }
+
+    onFiltered(filters){
+        this.requestForUsers(1, filters.username, filters.gender, filters.dob);
+        this.requestForCountUsers(filters.username, filters.gender, filters.dob);
+    }
+
+    onFetched(rab){
+        this.requestForUsers();
+        this.setState(function(state){
+            return {
+                userNumber: parseInt(state.userNumber) + parseInt(rab)
+            }
+        });
     }
 
     getNumberOfUser(res){
@@ -64,10 +88,14 @@ class Main extends React.Component{
     }
 
     showUpdateUserUI(idUser){
-        makeRequest('/users/' + idUser, 'GET', this.getUser);
+        makeRequest('/users/' + idUser, 'GET', this.getUserForUpdate);
     }
 
-    getUser(user){
+    showInfosUserUI(idUser){
+        makeRequest('/users/' + idUser, 'GET', this.getUserForInfos);
+    }
+
+    getUserForUpdate(user){
         user = JSON.parse(user);
 
         this.modal.current.setState({
@@ -80,9 +108,12 @@ class Main extends React.Component{
             update: true
         });
 
-        document.querySelector('#staticBackdropLabel').text = "Modifier un utilisateur";
-
         new bootstrap.Modal(document.getElementById('staticBackdrop'), {}).show();
+    }
+
+    getUserForInfos(user){
+        user = JSON.parse(user);
+        this.modalInfosUser.current.displayUser(user);
     }
     
     render(){
@@ -90,11 +121,12 @@ class Main extends React.Component{
             <div>
                 <Header />
                 <div id="main">
-                    <FilterBloc/>
+                    <FilterBloc numberOfUsers={this.state.userNumber} onFetched={this.onFetched} onFiltered={this.onFiltered}/>
                     <UserList 
                         users={this.state.users} 
                         numberUser={this.state.userNumber}
                         updateUserUI={this.showUpdateUserUI} 
+                        infosUserUI={this.showInfosUserUI}
                         onUserDeleted={this.removeUser}
                         onPaginate={this.requestForUsers}
                     />
@@ -104,6 +136,7 @@ class Main extends React.Component{
                         onUserUpdated={this.updateUser}
                         title="Modifier un utilisateur"
                     />
+                    <ShowUser ref={this.modalInfosUser}/>
                 </div>
             </div>
         );
