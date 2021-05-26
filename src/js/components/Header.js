e5849d8ad58434a61b57e96add1645f9b391a68b
Don TG <govanetsianga79@gmail.com>
2021-05-26 22:13:35 +0100

    change header text

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,10 +1,7 @@
 function Header(){
     return (
         <header>
-            <div>
-                <span>TP MERN</span>
-                produced by Don TSIANGA Govane
-            </div>
+            <div>Getting started React</div>
             <div className="image-bloc">
                 <img src="images/logo.png" alt="Don TG logo"/>
             </div>
